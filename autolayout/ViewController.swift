76b34e0016b48b521ef8771329ed972dfaafd919
Rohmats <rohmatsuseno@gmail.com>
2018-08-19 21:46:48 +0700

    add stackView: prefBtn, indicator, nextBtn

--- autolayout/ViewController.swift
+++ autolayout/ViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+extension UIColor {
+    static var currentPageIndicator = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
+    static var pageIndicator = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1)
+}
+
 class ViewController: UIViewController {
 
 //    let's avoid polluting viewDidLoad
@@ -27,8 +32,6 @@ class ViewController: UIViewController {
         attributedText.append(NSAttributedString(string: "\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 13), NSAttributedStringKey.foregroundColor: UIColor.gray]))
         textView.attributedText = attributedText
         
-//        textView.text = "Join us today in our fun and games!"
-//        textView.font = UIFont.boldSystemFont(ofSize: 18)
         textView.textAlignment = .center
         textView.isEditable = false
         textView.isScrollEnabled = false
@@ -36,15 +39,74 @@ class ViewController: UIViewController {
         return textView
     }()
     
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+        return button
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.currentPageIndicator, for: .normal)
+        return button
+    }()
+    
+    
+    
+    private let pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+//        let pinkColor = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
+        pc.currentPageIndicatorTintColor = .currentPageIndicator
+        pc.pageIndicatorTintColor = .pageIndicator
+        return pc
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        set image to subView
-//        view.addSubview(bearImageView)
         view.addSubview(descriptionTextView)
+        
+        setupBottomControls()
         
         setupLayout()
         
+    }
+    
+    private func setupBottomControls() {
+//        view.addSubview(previousButton)
+//        previousButton.backgroundColor = .red
+//        previousButton.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        
+//        let yellowView = UIView()
+//        yellowView.backgroundColor = .yellow
+        
+//        let greenView = UIView()
+//        greenView.backgroundColor = .green
+        
+//        let blueView = UIView()
+//        blueView.backgroundColor = .blue
+        
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton, pageControl, nextButton])
+        
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlsStackView.distribution = .fillEqually
+//        bottomControlsStackView.axis = .vertical
+        view.addSubview(bottomControlsStackView)
+        
+        NSLayoutConstraint.activate([
+            bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+            ])
     }
     
     private func setupLayout() {
