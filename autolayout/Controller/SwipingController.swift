02672eedbb704fcc7a6ae967c7fa8f16c9fa11e7
Rohmats <rohmatsuseno@gmail.com>
2018-09-02 11:52:48 +0700

    Landscape support

--- autolayout/Controller/SwipingController.swift
+++ autolayout/Controller/SwipingController.swift
@@ -51,7 +51,7 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
         collectionView?.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
     }
     
-    private lazy var pageControl: UIPageControl = {
+    lazy var pageControl: UIPageControl = {
         let pc = UIPageControl()
         pc.currentPage = 0
         pc.numberOfPages = pages.count
@@ -80,7 +80,6 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
     override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
         let x = targetContentOffset.pointee.x
         pageControl.currentPage = Int(x / view.frame.width)
-//        print(x, view.frame.width, x / view.frame.width)
     }
     
     override func viewDidLoad() {
@@ -89,36 +88,7 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
         setupBottomControls()
         
         collectionView?.backgroundColor = .white
-        collectionView?.register(PageCustomCell.self, forCellWithReuseIdentifier: "cellId")
+        collectionView?.register(PageCell.self, forCellWithReuseIdentifier: "cellId")
         collectionView?.isPagingEnabled = true
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return pages.count
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! PageCustomCell
-        
-        let page = pages[indexPath.item]
-        cell.page = page
-        
-//        private collectionView
-//        cell.bearImageView.image = UIImage(named: page.imageName)
-//        cell.descriptionTextView.text = page.headerText
-        
-//        let imageName = imageNames[indexPath.item]
-//        cell.bearImageView.image = UIImage(named: imageName)
-//        cell.descriptionTextView.text = headerStrings[indexPath.item]
-
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: view.frame.height)
     }
 }
---
+++ autolayout/Extension/SwipingControllerExtension.swift
@@ -0,0 +1,26 @@
+//
+//  SwipingContreollerExtension.swift
+//  autolayout
+//
+//  Created by Rohmat Suseno on 02/09/18.
+//  Copyright © 2018 InnoCent Bandung. All rights reserved.
+//
+
+import UIKit
+
+extension SwipingController {
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        coordinator.animate(alongsideTransition: { (_) in
+            self.collectionViewLayout.invalidateLayout()
+            if self.pageControl.currentPage == 0 {
+                self.collectionView?.contentOffset = .zero
+            } else {
+                let indexPath = IndexPath(item: self.pageControl.currentPage, section: 0)
+                self.collectionView?.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            }
+            
+        }) { (_) in
+            
+        }
+    }
+}
